ace2cb847ae9d5c302c7127245c36507a8bf349f
邓安月 <1434807906@qq.com>
2021-01-29 14:19:04 +0800

    完成部分支付页面

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,43 @@
-// pages/pay/index.js
+// pages/cart/index.js
+import {
+  getSetting,
+  openSetting,
+  chooseAddress,showModal,showToast
+} from '../../utils/asyncWx.js'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0,
   },
+  onShow() {
+    const address = wx.getStorageSync("address");
+    let cart = wx.getStorageSync("cart") || [];
+    cart=cart.filter(v=>v.checked)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    let totalPrice = 0
+    let totalNum = 0
+    cart.forEach(v => {
+        totalPrice += v.num * v.goods_price
+        totalNum += v.num
+    })
+    this.setData({
+      cart,
+      totalPrice,
+      totalNum,address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击支付
+  handleOrderPay(){
+    const token=wx.getStorageSync("token");
+    if(!token){
+      wx.navigateTo({
+        url: '/pages/auth/index',
+      });
+      return
+    }
+    console.log("token");
   }
 })
--- utils/asyncWx.js
+++ utils/asyncWx.js
@@ -68,3 +68,19 @@ export const showToast = ({title}) => {
   })
 
 })}
+
+
+// Promise login
+export const login = () => {
+  return new Promise((resolve, reject) => {
+  wx.login({
+    timeout:10000,
+    success: (result) => {
+      resolve(result)
+    },
+    fail: (err) => {
+      reject(err)
+    },
+  })
+
+})}
